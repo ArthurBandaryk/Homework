5a6012c2245d61cbc5217e14ce6f51ed5f609e1e
Arthur Bandaryk <arthur_bandaryk@mail.ru>
2021-01-28 16:25:27 +0300

    hw2

---
+++ 02-1-SDL-static/src/main.cxx
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include <SDL2/SDL_version.h>
+#include "Configure.h"
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        std::cout << "Version " << argv[0]	<< ": " << FirstSDLProj_VERSION_MAJOR
+                  << "." << FirstSDLProj_VERSION_MINOR << "."
+                  << FirstSDLProj_VERSION_PATCH << std::endl;
+    }
+
+    SDL_version sdl_compiled { 0, 0, 0 };
+    SDL_version sdl_linked { 0, 0, 0 };
+
+    SDL_VERSION(&sdl_compiled)
+    SDL_GetVersion(&sdl_linked);
+
+    std::cout << "Linked with static lib!!!" << std::endl;
+
+    std::cout << "Compiled version: "
+              << static_cast<int>(sdl_compiled.major) << "."
+              << static_cast<int>(sdl_compiled.minor) << "."
+              << static_cast<int>(sdl_compiled.patch) << std::endl;
+    std::cout << "Linked version of libs: "
+              << static_cast<int>(sdl_linked.major) << "."
+              << static_cast<int>(sdl_linked.minor) << "."
+              << static_cast<int>(sdl_linked.patch) << std::endl;
+
+    return  std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
